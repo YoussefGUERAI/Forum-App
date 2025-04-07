0fa447a2a299c325d97640e51025d7e54cad5d98
Anir Achibane <anirf42@gmail.com>
2025-04-07 12:04:36 +0100

    fixed initialisation errors

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -4,33 +4,20 @@
             <input type="email" required placeholder="Your email" v-model="email">
             <input type="password" required placeholder="Your password" v-model="password">
             <button>Login</button>
-            <div class="error">{{ error }}</div>
+            <div class="error"> </div>
         </form>
     </div>
   </template>
   
   <script setup>
-  import useLogin from '@/composables/useLogin';
+  /*import useLogin from '@/composables/useLogin'*/
  
 import { ref } from 'vue';
 
   
   const email = ref('')
   const password = ref('')
-  const {error, login} = useLogin()
 
-  const emit = defineEmits(['customEvent']);
-
-  const handleLogin = async () => {
-      const response = await login(email.value, password.value)
-      if(!error.value){
-        console.log("USER LOGGED SUCCESS")
-        emit('login')
-      }else {
-        console.log("USER NOT LOGGED")
-      }
-
-  }
   </script>
   
 <style>
