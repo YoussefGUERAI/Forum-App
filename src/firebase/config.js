7e5d9c7e9c789586df62f148001d989006ea8817
mac <mac@MacBook-Pro-de-mac-7.local>
2025-04-07 10:37:05 +0100

    Init Project

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCtt3KQPERyBsO_3qOgilt35c_07Sx30Vg",
+    authDomain: "forum-app-abd19.firebaseapp.com",
+    projectId: "forum-app-abd19",
+    storageBucket: "forum-app-abd19.firebasestorage.app",
+    messagingSenderId: "992653024443",
+    appId: "1:992653024443:web:62b0afa3afc42b8ed15d9e"
+};
+
+firebase.initializeApp(firebaseConfig)
+
+const projectFirestore = firebase.firestore()
+
+export { projectFirestore }
