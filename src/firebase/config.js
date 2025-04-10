c05e5bc45b12a4b13b9950bc9277a6f02874fe21
mac <mac@MacBook-Pro-de-mac-7.local>
2025-04-10 19:42:35 +0100

    Initial signup and login pages

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,17 +1,21 @@
-import firebase from 'firebase/app'
-import 'firebase/firestore'
+
+// Firebase v8 syntax
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCtt3KQPERyBsO_3qOgilt35c_07Sx30Vg",
-    authDomain: "forum-app-abd19.firebaseapp.com",
-    projectId: "forum-app-abd19",
-    storageBucket: "forum-app-abd19.firebasestorage.app",
-    messagingSenderId: "992653024443",
-    appId: "1:992653024443:web:62b0afa3afc42b8ed15d9e"
+  apiKey: "AIzaSyCtt3KQPERyBsO_3qOgilt35c_07Sx30Vg",
+  authDomain: "forum-app-abd19.firebaseapp.com",
+  projectId: "forum-app-abd19",
+  storageBucket: "forum-app-abd19.firebasestorage.app",
+  messagingSenderId: "992653024443",
+  appId: "1:992653024443:web:62b0afa3afc42b8ed15d9e"
 };
 
-firebase.initializeApp(firebaseConfig)
+firebase.initializeApp(firebaseConfig);
 
-const projectFirestore = firebase.firestore()
+const auth = firebase.auth();
+const db = firebase.firestore();
 
-export { projectFirestore }
+export { auth, db };
--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,16 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import SignupView from "@/views/SignupView.vue";
+import LoginView from "@/views/LoginView.vue";
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+  { path: "/signup", component: SignupView },
+  { path: "/login", component: LoginView },
+  // other routes
+];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  history: createWebHistory(),
+  routes,
+});
 
-export default router
+export default router;
